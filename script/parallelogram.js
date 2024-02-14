3bd1205f656c22e3622afb3e15110d45a38030df
Ayub40 <ayubk4028@gmail.com>
2024-02-14 14:15:37 +0600

    add

---
+++ script/parallelogram.js
@@ -0,0 +1,45 @@
+function calculateParallelogramArea() {
+    // const baseInput = document.getElementById('parallelogram-base');
+    // const baseText = baseInput.value;
+    // const base = parseFloat(baseText);
+    // console.log(base);
+
+
+    const base = getElementById();
+    console.log(base);
+}
+
+
+// function getParallelogramBase() {
+//     const baseInput = document.getElementById('parallelogram-base');
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     return base;
+// }
+
+
+function calculateParallelogramArea() {
+    
+    const base = getInputValueById('parallelogram-base');
+    console.log('base value', base);
+
+    const height = getInputValueById('parallelogram-height');
+    console.log('height value', height);
+
+    const area = base * height;
+    console.log('area of the parallelogram is ', area);
+
+    setInnerTextById('parallelogram-area', area);
+}
+
+function getInputValueById(inputField) {
+    const inputField = document.getElementById(inputField);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    console.log(inputValue);
+}
+
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
